4d44eaafbd1357d55fc8902a7f8fa4fce6a7ee87
andrea ilieschi <andrea.ilieschi@gmail.com>
2021-07-06 15:03:31 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Link } from 'react-router-dom'
 
 import './App.css';
 
 
 
 import Navbar from './components/Navbar';
-import CountriesList from './components/CountriesList';
+// import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
-import countries from './countries.json';
+// import countries from './countries.json';
+
 
 class App extends React.Component {
+  state = {
+    countries: []
+  }
   
-
-
+componentDidMount() {
+    fetch('https://raw.githubusercontent.com/mledoze/countries/master/countries.json')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ countries: data })
+      console.log(this.state.countries)
+    })
+    .catch(console.log)
+  }
+  
   render () {
+    
   return (
     <>
     <div className="App">
@@ -22,7 +35,17 @@ class App extends React.Component {
     </div>
       
       <div className="container">
-      <CountriesList />
+      <div>
+      {this.state.countries.map((country) => {
+        return (
+        <p key={country.cca3}>
+        <Link to={`/${country.cca3}`} style={{ textDecoration: 'none' }}>
+
+        {country.flag} {country.name.common}</Link></p>
+      )
+      })}
+      </div>
+      
       
       <Switch>
       <Route path='/:id' component={CountryDetails} />
@@ -40,3 +63,33 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+// class App extends React.Component {
+  
+//   render () {
+//   return (
+//     <>
+//     <div className="App">
+//       <Navbar name="Lab-WikiCountries" />
+//     </div>
+      
+//       <div className="container">
+//       <CountriesList />
+      
+//       <Switch>
+//       <Route path='/:id' component={CountryDetails} />
+    
+      
+//       </Switch>
+  
+//       </div>
+
+
+    
+//     </>
+//   );
+//   }
+// }
+
+// export default App;
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
+// import React from 'react';
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import countries from '../countries.json'
+// import countries from '../countries.json'
 
-class CountriesList extends React.Component {
+// class CountriesList extends React.Component {
     
 
-  // componentDidMount() {
-  //   fetch('https://raw.githubusercontent.com/mledoze/countries/master/countries.json')
-  //   .then(res => res.json())
-  //   .then((data) => {
-  //     this.setState({ countries: data })
-  //     console.log(this.state.countries)
-  //   })
-  //   .catch(console.log)
-  // }
-    render() {
-        return (
+//   componentDidMount() {
+//     fetch('https://raw.githubusercontent.com/mledoze/countries/master/countries.json')
+//     .then(res => res.json())
+//     .then((data) => {
+//       this.setState({ countries: data })
+//       console.log(this.state.countries)
+//     })
+//     .catch(console.log)
+//   }
+//     render() {
+//         return (
             
-            <div>
+//             <div>
             
-                {countries.map((country) => {
-                return <p key={country.cca3}><Link to={`/${country.cca3}`} style={{ textDecoration: 'none' }}>{country.flag} {country.name.common}</Link></p>
+//                 {countries.map((country) => {
+//                 return <p key={country.cca3}><Link to={`/${country.cca3}`} style={{ textDecoration: 'none' }}>{country.flag} {country.name.common}</Link></p>
                                 
-            })}
-            </div>
+//             })}
+//             </div>
             
-        )
-    }
-}
+//         )
+//     }
+// }
 
-export default CountriesList
+// export default CountriesList
